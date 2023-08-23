baab6bd40cfa0031601c418cb66b907cc5a269e6
ahmed mahmoud <fiverr119988@gmail.com>
2023-08-23 12:26:40 +0300

    edit

--- app/Domains/JournalEntry/Resources/CollectionBalanceSheetResource.php
+++ app/Domains/JournalEntry/Resources/CollectionBalanceSheetResource.php
@@ -13,6 +13,14 @@ class CollectionBalanceSheetResource extends JsonResource
         return [
             'assets' => new BalanceSheetResource($this['assets']),
             'liabilities' => new BalanceSheetResource($this['liabilities']),
+            'liabilities_plus_assets' =>
+                $this['assets']?->groups?->pluck('accounts')->flatten()->sum('opening_balance') +
+                $this['assets']?->groups?->pluck('accounts')->flatten()->pluck('journalEntryDetail')->flatten()->sum('credit') -
+                $this['assets']?->groups?->pluck('accounts')->flatten()->pluck('journalEntryDetail')->flatten()->sum('debit')
+                +$this['liabilities']?->groups?->pluck('accounts')->flatten()->sum('opening_balance') +
+                $this['liabilities']?->groups?->pluck('accounts')->flatten()->pluck('journalEntryDetail')->flatten()->sum('credit') -
+                $this['liabilities']?->groups?->pluck('accounts')->flatten()->pluck('journalEntryDetail')->flatten()->sum('debit')
+            ,
             'equity' => new BalanceSheetResource($this['equity']),
         ];
     }
