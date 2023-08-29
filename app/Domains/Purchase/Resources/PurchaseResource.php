b8da38b81bb097040277272c7ca09991bcfc04ea
ahmed mahmoud <fiverr119988@gmail.com>
2023-08-29 22:19:17 +0300

    fix

--- app/Domains/Purchase/Resources/PurchaseResource.php
+++ app/Domains/Purchase/Resources/PurchaseResource.php
@@ -12,17 +12,17 @@ class PurchaseResource extends JsonResource
 
 
         return [
-            'id' => $this->id,
+            'id'             => $this->id,
             'invoice_number' => $this->invoice_number,
-            'total' => $this->total,
-            'date' => $this->date,
-            'creator' => $this->creator,
-            'products' => $this->products,
-            'stock' => $this->stock,
-            'purchasable' => $this->purchasable,
-            'taxes' => $this->taxes,
-            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
-            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
+            'total'          => $this->total,
+            'date'           => $this->date,
+            'creator'        => $this->creator,
+            'products'       => $this->products,
+            'stock'          => $this->stock,
+            'purchasable'    => $this->purchasable,
+            'taxes'          => $this->taxes,
+            'created_at'     => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at'     => $this->updated_at->format('Y-m-d H:i:s'),
         ];
     }
 }
