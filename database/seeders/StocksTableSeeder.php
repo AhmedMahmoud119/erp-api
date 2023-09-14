40ede529ac750269264fd754cd5e151b0b0fbbe5
mahmmoudmohammed <mahmoud.mohamed@gmail.com>
2023-09-14 13:34:07 +0300

    Stock Factory

--- database/seeders/StocksTableSeeder.php
+++ database/seeders/StocksTableSeeder.php
@@ -6,6 +6,7 @@ use App\Domains\Product\Models\Product;
 use App\Domains\Stock\Models\Stock;
 use App\Domains\User\Models\User;
 use App\Domains\Warehouse\Models\Warehouse;
+use Database\Factories\StockFactory;
 use Illuminate\Database\Seeder;
 
 class StocksTableSeeder extends Seeder
@@ -17,35 +18,6 @@ class StocksTableSeeder extends Seeder
      */
     public function run()
     {
-        $product = Product::first();
-        $warehouse = Warehouse::first();
-        $creator = User::first();
-
-        $data = [
-            [
-                'quantity' => rand(50, 200),
-                'opening_stock' => now(),
-                'selling_price' => rand(50, 200),
-                'purchasing_price' => rand(30, 150),
-                'product_id' => $product->id,
-                'warehouse_id' => $warehouse->id,
-                'creator_id' => $creator->id,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'quantity' => rand(50, 200),
-                'opening_stock' => now(),
-                'selling_price' => rand(50, 200),
-                'purchasing_price' => rand(30, 150),
-                'product_id' => $product->id,
-                'warehouse_id' => $warehouse->id,
-                'creator_id' => $creator->id,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-        ];
-        Stock::insert($data);
-
+        StockFactory::times(50)->create();
     }
 }
